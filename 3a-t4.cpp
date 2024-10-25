42c83176915dd76e09f1ff07bf62daf7700cdb47
cowplantyy <rurunastya1@gmail.com>
2024-10-25 12:14:26 +0300

    3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -2,31 +2,14 @@
 using namespace std;
 int main()
 {
-	int k, i = 1, sum_k = 0, number;
-	while (true)
-	{
-		cout << " Enter the amount of integers to sum = "; cin >> k;
-		if (k <= 0)
-		{
-			cout << "The operation is invalid. " << endl;
-			cin.clear();
-			cin.ignore(100, '\n');
-			continue;
-		}
-		break;
-	}
-	do
-	{
-		cout << "Enter integer nr. " << i << ": ";
-		cin >> number;
-		if (!cin)
-		{
-			cout << "The operation is invalid. " << endl; cin.clear(); cin.ignore(10000, '\n');
-			continue;
-		}
-		sum_k += number;
-		i++;
-	} while (i <= k);
-	cout << " The total sum of " << k << " integers is: " << sum_k;
-	return 0;
+int k, i = 1, sum_k = 0, number;
+cout << " Enter the amount of integers to sum = "; cin >> k;
+do {
+cout << "Enter integer nr. " << i << ": ";
+cin >> number;
+sum_k += number; i++;
+} while (i <= k);
+cout << " The total sum of " << k << " integers is: " << sum_k;
+return 0;
 }
+
